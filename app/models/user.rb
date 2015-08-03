b7f253c3fad3965a4031e46acbe64056d9977999
Jeff Ginsberg <jeffginsberg87@gmail.com>
2015-08-02 21:59:39 -0400

    secure user login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
 
+  before_save { self.email = email.downcase }
+
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
   validates :name, :email, presence: true
@@ -7,4 +9,6 @@ class User < ActiveRecord::Base
   validates :email, length: { maximum: 255 },
           format: { with: VALID_EMAIL_REGEX },
           uniqueness: { case_sensitive: false }
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
 end
