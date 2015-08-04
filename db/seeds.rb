67d2f6b8b9de8a710908bc432b9c2063cf99f1c3
Jeff Ginsberg <jeffginsberg87@gmail.com>
2015-08-04 18:03:38 -0400

    rake seed database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create!(name:  "Example User",
+             email: "example@hooked.org",
+             password:              "foobar",
+             password_confirmation: "foobar")
+
+19.times do |n|
+  name  = Faker::Name.name
+  email = "example-#{n+1}@hooked.org"
+  password = "password"
+  User.create!(name:  name,
+               email: email,
+               password:              password,
+               password_confirmation: password)
+end
